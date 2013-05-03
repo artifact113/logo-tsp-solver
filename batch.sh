23ede3343c054f1e01c175eff3dc4119cef87aed
kamil.rocki <kamil.rocki@79db9d8e-4ca2-0b89-6420-df117473bba4>
2013-05-03 07:21:29 +0000

    updated headers

--- batch.sh
+++ batch.sh
@@ -1,7 +1,9 @@
 for a in `seq 1000`
 do
-	./logo --in="./TSPLIB/usa13509.tsp" --initMethod="mf" --mode="cuda" --err=0 --timelimit=10800 --showLOInfo=1 --device=0 --comm=1 --out="usa13509_AT53_TITAN_x1"
-	./logo --in="./TSPLIB/usa13509.tsp" --initMethod="mf" --mode="cuda" --err=0 --timelimit=10800 --showLOInfo=1 --comm=1 --autoDevice --maxDevices=2 --out="usa13509_AT53_TITAN_x2"
-#	./logo --in="./TSPLIB/usa13509.tsp" --initMethod="mf" --mode="cuda" --err=0 --timelimit=3600 --showLOInfo=1 --comm=1 --autoDevice --maxDevices=3 --out="usa13509_AT53_TITAN_x3"
-	./logo --in="./TSPLIB/usa13509.tsp" --initMethod="mf" --mode="cuda" --err=0 --timelimit=10800 --showLOInfo=1 --comm=1 --autoDevice --maxDevices=4 --out="usa13509_AT53_TITAN_x4"
+
+	./logo --in="./TSPLIB/sw24978.tsp" --initMethod="mf" --mode="cpupar" --maxCoresCPU=32 --err=0 --timelimit=86400 --showLOInfo=1 --device=0 --comm=1 --out="sw24978_E5_2690_CPUPAR_32th_v062"
+	./logo --in="./TSPLIB/sw24978.tsp" --initMethod="mf" --mode="cpuhybrid" --maxCoresCPU=32 --err=0 --timelimit=86400 --showLOInfo=1 --device=0 --comm=1 --out="sw24978_E5_2690_CPUHYBRID_COMM_32th_v062"
+	./logo --in="./TSPLIB/sw24978.tsp" --initMethod="mf" --mode="cpuhybrid" --maxCoresCPU=32 --err=0 --timelimit=86400 --showLOInfo=1 --device=0 --comm=0 --out="sw24978_E5_2690_CPUHYBRID_NOCOMM_32th_v062"
+	./logo --in="./TSPLIB/sw24978.tsp" --initMethod="mf" --mode="cpu" --maxCoresCPU=1 --err=0 --timelimit=86400 --showLOInfo=1 --device=0 --comm=1 --out="sw24978_E5_2690_AVX_seq_v062"
+
 done
